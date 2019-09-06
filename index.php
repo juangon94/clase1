dd08a69de7796bf70beb6302f16e1b7ff8902cce
juangon94 <55006389+juangon94@users.noreply.github.com>
2019-09-06 19:52:20 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title> Pagina 1
+</title>
+</head>
+<body>
+<form action="sumar.php">
+<input type="text" name="num1" placeholder="ingrese un numero"><br><br>
+<input type="text" name="num2" placeholder="ingrese un numero"><br><br>
+<input type="submit" value="sumar">
+
+
+
+
+</form>
+</body>
+
+
+</html>
---
+++ sumar.php
@@ -0,0 +1,4 @@
+<?php
+echo "El resultado de la suma es:<br>";
+echo $_GET["num1"]  + $_GET["num2"];
+?>
